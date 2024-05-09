11cafd2e2db9815fd77202027bace440d26cb2d1
MindOfBear <ovidiunoc@gmail.com>
2024-05-09 22:48:07 +0300

    updated ex2

--- ex2.php
+++ ex2.php
@@ -1,22 +1,27 @@
 <?php
 
-$graf = array("a" => ["c"], "b" => ["c", "e"], "c" => ["a", "b"], "d" => ["c"], "e" => ["c", "b"], "f" => [], "g" => ["e", "a"]);
+$graf = array("a" => ["b"],
+"b" => ["e", "c"],
+"c" => ["b", "a"],
+"d" => ["c"],
+"e" => ["c", "b"],
+"f" => ["a", "b"],
+"g" => ["e", "a"]);
 $vizit = array();
 
-function algo($graf, $start) {
+function acc($graf, $start) {
     $Q = array();
     $Q[] = $start;
-    global $vizit;
     $vizit[] = $start;
     $Q_prev = $Q;
 
     while (true) {
         $Q_next = array();
         foreach ($Q_prev as $nod) {
-            foreach ($graf[$nod] as $adj) {
-                if (!in_array($adj, $Q) && !in_array($adj, $vizit)) {
-                    $Q_next[] = $adj;
-                    $vizit[] = $adj;
+            foreach ($graf[$nod] as $vec) {
+                if (!in_array($vec, $Q) && !in_array($vec, $vizit)) {
+                    $Q_next[] = $vec;
+                    $vizit[] = $vec;
                 }
             }
         }
@@ -30,5 +35,5 @@ function algo($graf, $start) {
     return array_keys(array_diff_key($graf, array_flip($Q)));
 }
 
-print_r(algo($graf, "a"));
+print_r(acc($graf, "a"));
 ?>
